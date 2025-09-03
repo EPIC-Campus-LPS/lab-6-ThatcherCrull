7b132619cb76353c706e460ca4ecb50c9ce22075
ThatcherCrull <crullthatcher@gmail.com>
2025-09-03 10:04:26 -0600

    Updated version

--- src/part2/Runner.java
+++ src/part2/Runner.java
@@ -11,19 +11,23 @@ public class Runner {
 			double suma = 0;
 			Scanner scan = new Scanner(file);
 			while (scan.hasNextLine()) {
-				String line = scan.nextLine();
-				int i = 0;
+				String line = scan.nextLine().trim();
+				Scanner lineScanner = new Scanner(line);
                 if (line.contains("campsite")) {
-                	i = line.indexOf(" ");
-                	Campsite camp = new Campsite(i + 1);
+                	lineScanner.next();
+                	double r = lineScanner.nextDouble();
+                	Campsite camp = new Campsite(r);
                 	sump += camp.perimeter();
                 	suma += camp.area();
                 } else if (line.contains("trail")) {
-                	i = line.indexOf(" ");
-                	Trail t = new Trail((i + 1), (i + 1));
+                	lineScanner.next();
+                	double l = lineScanner.nextDouble();
+                	double w = lineScanner.nextDouble();
+                	Trail t = new Trail(l, w);
                 	sump += t.perimeter();
                 	suma += t.area();
                 }
+                lineScanner.close();
 			}
 			System.out.println("Park Perimeter: "+ sump);
 			System.out.print("Park Area: "+suma);
